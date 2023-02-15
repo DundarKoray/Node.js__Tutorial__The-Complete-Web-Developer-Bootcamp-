a2acdaa981d9c1abdb702fea434946cef33d4688
Koray Dundar <koray.dundar@exove.com>
2023-02-15 17:25:47 +0200

    completed section 3

--- section-3: Node Express/app.js
+++ section-3: Node Express/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+// console.log('hello world')
+
+//if you navigate to localhost:3000/hello it will print "Hello world"
+app.get('/hello', function(req, res){
+    res.send('Hello world');
+});
+
 app.listen(3000, function(){
     console.log('Server is running on PORT 3000')
-})
+});
